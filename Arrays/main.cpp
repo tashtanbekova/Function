86de2660ae739c6b5bea49de592fe98c1e0d69ce
Tashtanbekova <tashtanbekovaaynazik@gmail.com>
2024-03-19 22:46:27 +0300

    array functions in progres......

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 
 using namespace std;
+
 #define tab "\t"
+
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void Print(const int arr[], const int n);
+int Sum(const int arr[], const int n);
+double Avg(const int arr[], const int n);
+int minValue(const int arr[], const int n);
+int maxValue(const int arr[], const int n);
+int ShiftLeft(const int arr[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+
 	const int n = 5;
 	int arr[n];
 
-	int minRand, maxRand;
+	/*int minRand, maxRand;
 	do
 	{
 		cout << "Введите минимальное возможное случайное число: "; cin >> minRand;
 		cout << "Введите максимальное  возможное  случайное число: "; cin >> maxRand;
 		if (minRand == maxRand)cout << "Предела диапозоныне  не должны совпадать:  " << endl;
-	} while (minRand == maxRand);
+	} while (minRand == maxRand);*/
+
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Cреднее-арифметическое элементов числа: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение в массиве: " << minValue(arr, n) << endl;
+	cout << "Максимальное значение в массиве: " << maxValue(arr, n) << endl;
+	int number_of_shifts;
+	cout << "Введите насколько элементов выдвинуть на лево: "; cin >> number_of_shifts;
+
+}
+	
+void FillRand(int arr[], const int n, int minRand, int maxRand)
+{
 	if (maxRand < minRand)
 	{
 		int buffer = minRand;
@@ -24,12 +48,43 @@ void main()
 	}
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = minRand + rand() % (maxRand = minRand);
+		arr[i] = minRand + rand() % (maxRand - minRand);
 	}
-	//Вывод массива на экран
+}
+//Вывод массива на экран
+void Print(const int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+}
+
+int Sum(const int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Avg(const int arr[], const int n)
+{
+	return (double) Sum(arr, n) / n;
+}
+int minValue(const int arr[], const int n)
+{
+	return arr[0];
+}
+int maxValue(const int arr[], const int n)
+{
+	return arr[4];
+}
+int ShiftLeft(const int arr[], const int n)
+{
+	int number_of_shifts;
+
+	return arr[n];	
 }
