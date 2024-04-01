5e7dd484e58a2fa85c34c7d211e8a6df9a1eb974
Tashtanbekova <tashtanbekovaaynazik@gmail.com>
2024-04-01 07:33:52 +0300

    All DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,27 +6,35 @@ using namespace std;
 
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(char arr[], const int n, int minRand = 0, int maxRand = 100);
 
 void Print(const int arr[], const int n);
 void Print(const double arr[], const int n);
+void Print(const char arr[], const int n);
 
 int Sum(const int arr[], const int n);
 double Sum(const double arr[], const int n);
+char Sum(const char arr[], const int n);
 
 int Avg(const int arr[], const int n);
 double Avg(const double arr[], const int n);
+char Avg(const char arr[], const int n);
 
 int minValue(const int arr[], const int n);
 double minValue(const double arr[], const int n);
+char minValue(const char arr[], const int n);
 
 int maxValue(const int arr[], const int n);
 double maxValue(const double arr[], const int n);
+char maxValue(const char arr[], const int n);
 
-int ShiftLeft(int arr[], const int n);
-double ShiftLeft(double arr[], const int n);
+int ShiftLeft(int arr[], const int n );
+double ShiftLeft(double arr[], const int n );
+char ShiftLeft(char arr[], const int n );
 
 int ShiftRight(int arr[], const int n);
 double ShiftRight (double arr[], const int n);
+char ShiftRight (char arr[], const int n);
 
 void main()
 {
@@ -50,7 +58,8 @@ void main()
 	cout << "Минимальное значение в массиве: " << minValue(arr, n) << endl;
 	cout << "Максимальное значение в массиве: " << maxValue(arr, n) << endl;
 	int number_of_shifts;
-	//cout << "Введите насколько элементов выдвинуть на лево: "; cin >> number_of_shifts;
+	cout << "Введите насколько элементов выдвинуть на лево: "; cin >> number_of_shifts;
+	cout << "Введите насколько элементов выдвинуть на право : "; cin >> number_of_shifts;
 
 	const int m = 8;
 	double d_arr[m];
@@ -87,6 +96,22 @@ void FillRand(double arr[], const int n, int minRand, int maxRand)
 		arr[i] /= 100;
 	}
 }
+void FillRand(char arr[], const int n, int minRand, int maxRand)
+{
+	if (maxRand < minRand)
+	{
+		int buffer = minRand;
+		minRand = maxRand;
+		maxRand = buffer;
+	}
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = minRand + rand() % (maxRand - minRand);
+		arr[i] /= 100;
+	}
+}
 
 //Вывод массива на экран
 void Print(const int arr[], const int n)
@@ -98,6 +123,14 @@ void Print(const int arr[], const int n)
 	cout << endl;
 }
 void Print(const double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Print(const char arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -124,6 +157,15 @@ double Sum(const double arr[], const int n)
 	}
 	return sum;
 }
+char Sum(const char arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
 
 int Avg(const int arr[], const int n)
 {
@@ -133,41 +175,173 @@ double Avg(const double arr[], const int n)
 {
 	return (double) Sum(arr, n) / n;
 }
+char Avg(const char arr[], const int n)
+{
+	return (double) Sum(arr, n) / n;
+}
 
 int minValue(const int arr[], const int n)
 {
-	return arr[0];
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)
+			min = arr[i];
+     }
+	return min;
 }
 double minValue(const double arr[], const int n)
 {
-	return arr[0];
+		double min = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			if (arr[i] < min)
+				min = arr[i];
+		}
+		return min;
+}
+char minValue(const char arr[], const int n)
+{
+		char min = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			if (arr[i] < min)
+				min = arr[i];
+		}
+		return min;
 }
 
 int maxValue(const int arr[], const int n)
 {
-	return arr[4];
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < max)
+			max = arr[i];
+	}
+	return max;
 }
 double maxValue(const double arr[], const int n)
 {
-	return arr[4];
+	double max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < max)
+			max = arr[i];
+	}
+	return max;
+}
+char maxValue(const char arr[], const int n)
+{
+	char max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < max)
+			max = arr[i];
+	}
+	return max;
 }
 
-int ShiftLeft(const int arr[], int n)
+int ShiftLeft( int arr[], const int n)
 {
-
+	for (int i = 0; i > 0; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
+}
+double ShiftLeft(double arr[], const int n)
+{
+	for (int i = 0; i > 0; i++)
+	{
+		double buffer = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
+}
+char ShiftLeft(char arr[], const int n)
+{
+	for (int i = 0; i > 0; i++)
+	{
+		char buffer = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
 }
 
-double ShiftLeft(const double arr[], int n)
+int ShiftRight(int arr[], const int n)
 {
-	int number_of_shifts;
-
-	return arr[n];	
+	for (int i = 0; i > 0; i++)
+	{
+		int buffer = arr[n-1];
+		for (int i = n-2; i>=n; i--)
+		{
+			arr[i+1] = arr[i];
+		}
+		arr[0] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
 }
-int ShiftRight( int arr[], int n)
+double ShiftRight(double arr[], const int n)
 {
-
+	for (int i = 0; i > 0; i++)
+	{
+		char buffer = arr[n-1];
+		for (int i = n-2; i>=n; i--)
+		{
+			arr[i+1] = arr[i];
+		}
+		arr[0] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
 }
-double ShiftRight(double arr[], int n)
+char ShiftRight(char arr[], const int n)
 {
-
+	for (int i = 0; i > 0; i++)
+	{
+		char buffer = arr[n-1];
+		for (int i = n-2; i>=n; i--)
+		{
+			arr[i+1] = arr[i];
+		}
+		arr[0] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
 }
