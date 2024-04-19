fd4f3abaea1aaa37c6748bd9b337f4674acec5d2
Ainazik Tashtanbekova <157056073+tashtanbekova@users.noreply.github.com>
2024-04-19 20:38:45 +0300

    All DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,6 +6,8 @@
 #include"Print.cpp"
 #include"Statistics.h"
 #include"Statistics.cpp"
+#include"Shifts.h"
+#include"Shifts.cpp"
 
 void main()
 {
@@ -20,8 +22,13 @@ void main()
 	cout << "Cреднее-арифметическое элементов числа: " << Avg(arr, n) << endl;
 	cout << "Минимальное значение в массиве: " << minValue(arr, n) << endl;
 	cout << "Максимальное значение в массиве: " << maxValue(arr, n) << endl;
-	//int number_of_shifts;
-	//cout << "Введите насколько элементов выдвинуть на лево: "; cin >> number_of_shifts;
+	int number_of_shifts;
+	cout << "Введите насколько элементов сдвинуть на лево: "; cin >> number_of_shifts;
+	ShiftLeft(arr, n, number_of_shifts);
+	Print(arr, n);
+	cout << "Введите насколько элементов сдвинуть направо: "; cin >> number_of_shifts;
+	ShiftRight(arr, n, number_of_shifts);
+	Print(arr, n);
 
 	const int D_SIZE = 8;
 	double d_arr[D_SIZE];
@@ -31,5 +38,11 @@ void main()
 	cout << "Cреднее-арифметическое элементов числа: " << Avg(d_arr, D_SIZE) << endl;
 	cout << "Минимальное значение в массиве: " << minValue(d_arr, D_SIZE) << endl;
 	cout << "Максимальное значение в массиве: " << maxValue(d_arr, D_SIZE) << endl;
+	cout << "Введите насколько элементов сдвинуть налево: "; cin >> number_of_shifts;
+	ShiftLeft(d_arr, D_SIZE, number_of_shifts);
+	Print(d_arr, D_SIZE);
+	cout << "Введите насколько элементов сдвинуть направо: "; cin >> number_of_shifts;
+	ShiftRight(d_arr, D_SIZE, number_of_shifts);
+	Print(d_arr, D_SIZE);
 }
 
